9941a4f84a369fafe8ee0b63f288ef2a3d35560d
GC <grzes.cichosz@gmail.com>
2021-03-16 12:48:41 +0100

    added the testing script

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function ok {
+    echo -e '\t' $1 ok!
+}
+function bad {
+    echo -e '\t' $1 nie ok :'('
+}
+
+if [ $# -lt 2 ]
+then
+    echo poprawne wywołanie: ./test.sh prog dir
+else
+    prog=$1
+    dir=$2
+    tmpout=tmp_out
+    tmperr=tmp_err
+
+    rm $tmpout $tmperr 2>/dev/null
+    
+    for f in $dir/*.in
+    do
+        echo sprawdzam $f # $(basename $f)
+        ./$prog <$f >$tmpout 2>$tmperr
+        
+        cmp --silent ${f%in}out $tmpout && ok out || bad out
+        
+        cmp --silent ${f%in}err $tmperr && ok err || bad err
+        
+        rm $tmpout $tmperr
+    done
+fi
+             
