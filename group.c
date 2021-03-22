469d51eccf5af5073f2efe49d98e67a96e918c27
GC <grzes.cichosz@gmail.com>
2021-03-23 00:30:22 +0100

    neat comparators

--- group.c
+++ group.c
@@ -35,8 +35,8 @@ void end_group(Group* group, Groups* all_groups)
 {
   size_t* new_group;
   /* długość tablicy indeksów musi być zwiększona o 1 ponieważ na ich końcu
-   * zapisywane zostaje 0, ze względu na brak zerowego wiersza, tym samym jest
-   * dobrym wyznacznikiem końca jednej grupy w tablicy. */
+   * zapisywane zostaje 0. względu na numeracje wierszy od 1 brak zerowego
+   * wiersza, tym samym jest 0 dobrym wyznacznikiem końca jednej grupy w tablicy. */
   size_t group_len = (group->used + 1) * sizeof(size_t);
   qsort(group->val, group->used, sizeof(size_t), cmp_size_t);
   new_group = (size_t*) malloc(group_len);
@@ -60,9 +60,7 @@ void print_all_groups(Groups all_groups)
 
   for (i = 0; i < all_groups.used; ++i) {
     /* pętla służy wypisaniu wszystkich podobnych wierszy jednej grupy, prócz
-     * ostatniego, ponieważ po nim nie nastąpi spacja, tylko nowa linia. Zatem
-     * warunek patrzy o jedno pole do przodu. Zero oznacza koniec grupy zgodnie
-     * z metodą opisaną w funkcji end_group. */
+     * ostatniego (po którym następuje 0), ponieważ po nim nie nastąpi spacja */
     for (j = 0; all_groups.val[i][j + 1] != 0; ++j)
       printf("%lu ", all_groups.val[i][j]);
 
@@ -88,7 +86,7 @@ static void find_similars(ParsedLine* plines, size_t len)
   if (len == 0)
     return;
 
-  /* Zakładam (może mylnie), że częściej zdarzy się wiele grup rozczłonkowanych
+  /* Zakładam (może mylnie), że częściej zdarza się wiele grup rozczłonkowanych
    * niżli jedna wielka, zatem domyslnie przeznaczam więcej miejsca na tablicę
    * wszystkich grup niż pojedynczą grupę. */
   array_init(&group, sizeof(size_t), SMALL_ARRAY);
@@ -137,7 +135,6 @@ void write_groups(ParsedText t)
   find_similars(t.val, t.used);
 }
 
-
 /* komparatory. Zasadniczo:
  *   cmp(a, b) = if a < b then -1 else if a > b then 1 else 0,
  * Porównując rzecz złożoną (np tablicę, struktury) pierwsza różnica decyduje
@@ -150,9 +147,10 @@ static int cmp_pline(const void* l1, const void* l2)
   int cmp;
 
   if (pl1.pwords.used != pl2.pwords.used)
-    return (pl1.pwords.used < pl2.pwords.used) ? (-1) : 1;
+    return (pl1.pwords.used < pl2.pwords.used) ? -1 : 1;
 
   for (size_t i = 0; i < pl1.pwords.used; ++i) {
+    /* pierwsza różnica decyduje ponieważ zał, że linie są posortowane */
     if ((cmp = cmp_pword(&pl1.pwords.val[i], &pl2.pwords.val[i])))
       return cmp;
   }
@@ -185,43 +183,34 @@ static int cmp_pword(const void* p1, const void* p2)
 
 /**
  *  Funkcja porównujące dwie liczby @a i @b całkowite typu @Whole.
- *  Najpierw po znaku (- < +), później po wartości absolutnej. */
+ *  Najpierw po znaku (- < +), później po wartości absolutnej (w przypadku znaku
+ *  MINUS porządek się obraca). */
 static int cmp_whole(Whole n1, Whole n2)
 {
-  /* porównanie co do znaku i odpowiedniej wartości absolutnej.
-   * test odejmowawczy odpada, ponieważ może przebić się zakres rozwalić jakoś.
-   * Przeto ifologia */
   if (n1.sign == n2.sign) {
-    if (n1.sign == MINUS) {
-      if (n1.abs != n2.abs)
-        return (n1.abs < n2.abs) ? 1 : (-1);
-      else
-        return 0;
-    } else {
-      if (n1.abs != n2.abs)
-        return (n1.abs < n2.abs) ? (-1) : 1;
-      else
-        return 0;
-    }
+    if (n1.sign == MINUS)
+      return n1.abs != n2.abs ? (n1.abs < n2.abs ? 1 : -1) : 0;
+    else
+      return n1.abs != n2.abs ? (n1.abs < n2.abs ? -1 : 1) : 0;
   } else
-    return (n1.sign == MINUS && n2.sign == PLUS) ? (-1) : 1;
+    return (n1.sign == MINUS && n2.sign == PLUS) ? -1 : 1;
 }
 
 static int cmp_real(double n1, double n2)
 {
-  /* test odejmowawczy nie sprawdziłby się, albowiem inf - inf = ? */
-  if (n1 != n2)
-    return (n1 < n2) ? (-1) : 1;
-  else
-    return 0;
+  return n1 != n2 ? (n1 < n2 ? -1 : 1) : 0;
 }
 
 static int cmp_size_t(const void* a, const void* b)
 {
-  return *(size_t*)a - *(size_t*)b;
+  size_t n1 = *(size_t*)a;
+  size_t n2 = *(size_t*)b;
+  return n1 != n2 ? (n1 < n2 ? -1 : 1) : 0;
 }
 
 static int cmp_size_t_p(const void* a, const void* b)
 {
-  return (**(size_t**)a) - (**(size_t**)b);
+  size_t n1 = **(size_t**)a;
+  size_t n2 = **(size_t**)b;
+  return n1 != n2 ? (n1 < n2 ? -1 : 1) : 0;
 }
