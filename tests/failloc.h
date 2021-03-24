a29b1c759d3c2d4a5e8a226c0fb8a448af801231
grzegorz <grzes.cichosz@gmail.com>
2021-03-25 00:51:36 +0100

    added calloc

--- tests/failloc.h
+++ tests/failloc.h
@@ -1,10 +1,12 @@
+
 #include <stdlib.h>
 #include <time.h>
 
 int sranded = 0;
 
-#define P 30
+#define P 50
 
 #define rnum() (!sranded ? (sranded=1, srand(time(NULL)), rand()%P) : rand()%P)
 #define malloc(len) rnum() ? malloc(len) : NULL
 #define realloc(ptr, len) rnum() ? realloc(ptr, len) : NULL
+#define calloc(count, len) rnum() ? calloc(count, len) : NULL
