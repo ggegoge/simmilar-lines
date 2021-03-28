a672105f49d7d4e02ea5a2b2b67dfcb5a2d1d165
grzegorz <grzes.cichosz@gmail.com>
2021-03-28 23:21:29 +0200

    less redundancy in the readme

--- src/main.c
+++ src/main.c
@@ -5,43 +5,31 @@
 
 /**
  * Wierszarz, Grzegorz Cichosz, marzec 2021
- *
+ * 
  * RIDMI
  *  Program dzieli się na kilka kroków i zagnieżdżonych podkroków. Te główne są
  *  dość dobrze wyróżnione w mainie.
- *
- * MODUS OPERANDI:
+ * 
+ * MODUS OPERANDI
  *   - inicjalizacja tablicy dynamicznej rodem z modułu `array' przechowującej
  *     sparsowany tekst
  *   - wczytanie tekstu z stdinu (moduł `input')
- *     + moduł input woła bezpośrednio po każdej linii ze stdinu moduł `parse'
- *       i jegoż funkcję `parse_line', która próbuje wczytać słowa z linii jako
- *       `ParsedLine'y' i dodać je do wspomnianej tablicy `ParsedText'
- *     + w razie komentarnej lub błędnej linii nie wczytuje jej
- *     + po tej części programu tablica `text' powinna być stosownie wypełniona
- *   - moduł group i jego eksportowa funkcja `write_groups' znajdują podobne
- *     wiersze i wypluwają na stdout
+ *     + moduł `input' woła bezpośrednio po każdej linii ze stdinu moduł `parse'
+ *       i jegoż funkcję `parse_line', która próbuje wczytać słowa z wiersza
+ *       do wspomnianej tablicy
+ *   - moduł `group' i funkcja `write_groups' znajdują grupy podobnych
+ *     i wypluwają na stdout
  *     + każda linia ma tablicę swoich słów -- `group' sortuje linie wewnątrz
- *       nich samych, aby podobne miały takie same tablicowe reprezentacje
- *     + następnie dochodzi do sortu wszystkich linii po tychże tablicach
- *       wewnętrznych, aby z tej postaci wyłuskać duplikaty -- tym samym
- *       zdobywamy poszukiwane tablice podobnych wierszy
- *       - wszelkie sortowanie wymaga funkcji porządkujących: moduł `compare'
- *
+ *       nich samych, aby podobne miały identyczne tablicowe reprezentacje
+ *     + następnie sortowanie wszystkich linii po tychże tablicach
+ *       wewnętrznych, aby z tej postaci wyłuskać duplikaty (tj podobne wiersze)
+ *       - wszelkie sortowanie wymaga funkcji porządkujących z modułu `compare'
+ * 
  * ZŁOŻONOŚĆ
  *  Ze względu na wspomniane sortowania złożoność można szacować asymptotą od
  *  góry O(k log k), gdzie k to liczba znaków na wejściu.
- *
- * MODUŁY
- *    (a) `array' -- implementacja dynamicznych tablic, chyba z tej części
- *        jestem najbardziej dumny. przewijają się w prawie każdym aspekcie
- *        rozwiązania, a jednocześnie liczba alokacji jest chyba przyzwoita
- *    (b) `input'
- *    (c) `parse'
- *    (d) `group'
- *    (c) `compare'
- *
  */
+
 
 int main(void)
 {
